6aab3e01efa41035a6735e229ad60a651e8891c2
Gideon <145397637+GemCollector11@users.noreply.github.com>
2025-12-03 16:44:46 -0500

    day 2

--- App.java
+++ App.java
@@ -1,17 +1,31 @@
 import java.util.List;
 import helper.ResourceHelper;
 import day1.day1Solve;
+import day2.day2solve;
 
 public class App {
 
     public static void main(String[] args) {
-        List<String> lines = ResourceHelper.loadLinesFromFile("day1input.txt");
+        day2solver();
+    }
+
+    public static void day2solver() {
+        List<String> lines = ResourceHelper.loadLinesFromFile("day2/day2sample.txt");
+
+        day2solve day2 = new day2solve();
+
+        long result = day2.solvepart2(lines);
+
+        System.out.println("4174379265, " + result);
+    }
+
+    public static void day1solver() {
+        List<String> lines = ResourceHelper.loadLinesFromFile("day1/day1input.txt");
 
         day1Solve day1 = new day1Solve();
 
         int result = day1.solvepart2(lines);
 
         System.out.println(result);
-
     }
 }
---
+++ day2solve.java
@@ -0,0 +1,152 @@
+package day2;
+
+import java.util.*;
+
+public class day2solve {
+
+    public final long solvepart1(List<String> input) {
+        long count = 0;
+        long minval = 0;
+        long maxval = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+
+            minval = Long.parseLong(input.get(i).replace(",", "").split("-")[0]);
+            maxval = Long.parseLong(input.get(i).replace(",", "").split("-")[1]);
+
+            for (Long k = Math.abs(minval); k <= Math.abs(maxval); k++) {
+
+                String value = k.toString();
+                int lengthOfSubval = (value.length()) / 2;
+                String subval1 = value.substring(0, lengthOfSubval);
+                String subval2 = value.substring(lengthOfSubval, value.length());
+
+                if (subval1.startsWith("0")) {
+
+                    count += Long.parseLong(value);
+                } else if (subval1.equals(subval2)) {
+
+                    count += Long.parseLong(value);
+                }
+
+            }
+
+        }
+
+        return count;
+    }
+
+    public final long solvepart2(List<String> input) {
+        long count = 0;
+        long minval = 0;
+        long maxval = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+
+            minval = Long.parseLong(input.get(i).replace(",", "").split("-")[0]);
+            maxval = Long.parseLong(input.get(i).replace(",", "").split("-")[1]);
+
+            for (Long k = Math.abs(minval); k <= Math.abs(maxval); k++) {
+                String minId = input.get(i).replace(",", "").split("-")[0];
+                String maxId = input.get(i).replace(",", "").split("-")[1];
+
+                String value = k.toString();
+                boolean useDiv1 = true;
+                boolean useDiv2 = true;
+                boolean useDiv3 = true;
+                boolean useDiv4 = true;
+                boolean useDiv5 = true;
+                boolean isInvalid = true;
+
+                for (int j = 0; j <= value.length(); j++) {
+                    int div = 0;
+                    int div1 = 2;
+                    int div2 = 3;
+                    int div3 = 4;
+                    int div4 = 5;
+                    int div5 = 7;
+
+                    if (useDiv1 && value.length() % div1 == 0) {
+                        div = div1;
+                    } else if (useDiv2 && value.length() % div2 == 0) {
+                        div = div2;
+                    } else if (useDiv3 && value.length() % div3 == 0) {
+                        div = div3;
+                    } else if (useDiv4 && value.length() % div4 == 0) {
+                        div = div4;
+                    } else if (useDiv5 && value.length() % div5 == 0) {
+                        div = div5;
+                    } else {
+                        div = div1;
+                    }
+                    String[] numSet = new String[div];
+                    int lengthOfSubval = (value.length() / div);
+
+                    for (int l = 0; l < numSet.length; l++) {
+                        int len = l * lengthOfSubval;
+                        numSet[l] = value.substring(len, len + lengthOfSubval);
+
+                    }
+
+                    for (int m = 1; m < numSet.length; m++) {
+                        if (value.length() > 1) {
+                            if (numSet[m - 1].equals(numSet[m])) {
+                                isInvalid = true;
+                            } else {
+                                if (value.length() > 3) {
+                                    if (useDiv1) {
+                                        useDiv1 = false;
+                                        useDiv2 = true;
+                                        useDiv3 = true;
+                                        useDiv4 = true;
+                                        useDiv5 = true;
+                                    } else if (useDiv2) {
+                                        useDiv1 = false;
+                                        useDiv2 = false;
+                                        useDiv3 = true;
+                                        useDiv4 = true;
+                                        useDiv5 = true;
+                                    } else if (useDiv3) {
+                                        useDiv1 = false;
+                                        useDiv2 = false;
+                                        useDiv3 = false;
+                                        useDiv4 = true;
+                                        useDiv5 = true;
+                                    } else if (useDiv4) {
+                                        useDiv1 = false;
+                                        useDiv2 = false;
+                                        useDiv3 = false;
+                                        useDiv4 = false;
+                                        useDiv5 = true;
+                                    } else if (useDiv5) {
+                                        useDiv1 = true;
+                                        useDiv2 = true;
+                                        useDiv3 = true;
+                                        useDiv4 = true;
+                                        useDiv5 = true;
+                                    }
+                                } else if (value.length() == 3) {
+                                    if (useDiv1) {
+                                        useDiv1 = false;
+                                        useDiv2 = true;
+                                    }
+                                } else {
+                                    useDiv1 = true;
+                                }
+                                isInvalid = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (isInvalid) {
+                        count += k;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        return count;
+    }
+}
